c51ab040165247d92dd74dd509df34645d8b92b4
gyshin <gyshin@stealien.com>
2024-11-19 13:09:52 +0900

    feat: fix TMN, TMX, REH was not appear && add function to retrieve data based on user location
    Also, add logo image and change main font

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,22 @@ android {
 
     defaultConfig {
         applicationId = "com.example.todayweather"
-        minSdk = 30
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        register("release") {
+            storeFile = file("android-key.keystore")
+            storePassword = "950608"
+            keyAlias = "android-key"
+            keyPassword = "950608"
+            enableV4Signing = true
+        }
     }
 
     buildTypes {
@@ -24,6 +34,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs["release"]
         }
     }
     compileOptions {
@@ -41,6 +52,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
